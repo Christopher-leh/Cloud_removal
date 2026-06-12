afe1a5d24dc74e2966a7cdb152cd6a127ea180b0
Christopher-leh <c.lehner002@gmail.com>
2026-06-12 14:12:24 +0000

    batchnorm in decoder and fixed size

--- models.py
+++ models.py
@@ -1,9 +1,6 @@
 from torch import nn
 import torch
 
-
-
-
 # VAE-Architektur mit Encoder, Decoder und Reparameterisierungstrick -------------------------------------
 
 
@@ -23,12 +20,12 @@ class Encoder(nn.Module):
         )  # 64x64 -> 32x32
         self.batch_norm3 = nn.BatchNorm2d(128)
         self.conv4 = nn.Conv2d(
-            128, 248, kernel_size=4, stride=2, padding=1
+            128, 256, kernel_size=4, stride=2, padding=1
         )  # 32x32 -> 16x16
-        self.batch_norm4 = nn.BatchNorm2d(248)
+        self.batch_norm4 = nn.BatchNorm2d(256)
         self.leaky_relu = nn.LeakyReLU(0.2)
-        self.linear_mu = nn.Linear(248 * 16 * 16, latent_dim)
-        self.linear_logvar = nn.Linear(248 * 16 * 16, latent_dim)
+        self.linear_mu = nn.Linear(256 * 16 * 16, latent_dim)
+        self.linear_logvar = nn.Linear(256 * 16 * 16, latent_dim)
 
     def forward(self, cloudy, label):
         x = torch.cat((cloudy, label), dim=1)  # Concatenate along the channel dimension
@@ -51,29 +48,36 @@ def reparameterize(mu, logvar):
     eps = torch.randn_like(std)
     return mu + eps * std
 
+
 class Decoder(nn.Module):
     def __init__(self, latent_dim=128):
         super().__init__()
-        self.linear = nn.Linear(latent_dim, 248 * 16 * 16)
+        self.linear = nn.Linear(latent_dim, 256 * 16 * 16)
         self.conv1 = nn.ConvTranspose2d(
-            248 + 3, 128, kernel_size=4, stride=2, padding=1
+            256 + 3, 128, kernel_size=4, stride=2, padding=1
         )
+        self.batch_norm1 = nn.BatchNorm2d(128)
         self.conv2 = nn.ConvTranspose2d(128, 64, kernel_size=4, stride=2, padding=1)
+        self.batch_norm2 = nn.BatchNorm2d(64)
         self.conv3 = nn.ConvTranspose2d(64, 32, kernel_size=4, stride=2, padding=1)
+        self.batch_norm3 = nn.BatchNorm2d(32)
         self.conv4 = nn.ConvTranspose2d(32, 3, kernel_size=4, stride=2, padding=1)
         self.leaky_relu = nn.LeakyReLU(0.2)
         self.tanh = nn.Tanh()
 
     def forward(self, z, label):
         x = self.leaky_relu(self.linear(z))
-        x = x.view(-1, 248, 16, 16)
+        x = x.view(-1, 256, 16, 16)
         label = nn.functional.interpolate(
             label, size=(16, 16), mode="bilinear", align_corners=False
         )
         x = torch.cat((x, label), dim=1)  # Concatenate along the channel dimension
         x = self.leaky_relu(self.conv1(x))
+        x = self.batch_norm1(x)
         x = self.leaky_relu(self.conv2(x))
+        x = self.batch_norm2(x)
         x = self.leaky_relu(self.conv3(x))
+        x = self.batch_norm3(x)
         x = self.tanh(self.conv4(x))
         return x
 
@@ -96,7 +100,9 @@ class Autoencoder(nn.Module):
         output = self.decoder(z, cloudy)
         return output
 
+
 # --------------------------------------------------------------------------
 
 # GAN-Architektur mit Generator und Diskriminator -------------------------------------
 
+c
